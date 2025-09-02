8c12cc9efd439966e86fcec266f1a82f41470999
HuntOreo <hloreair@outlook.com>
2025-09-01 22:09:55 -0400

    Add objective layout

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log('Hello world!');
+/* 
+Main Goal:
+  Build a functional Tic Tac Toe game.
+  Use as little globally scoped code as possible.
+ 
+Requirements:
+  Use Factory Functions
+    For single instance objects like game-board, 
+    or displayContainer, use IIFE module patterns so that 
+    it cannot be reused to create additional instances.
+  
+  Store the game-board inside of an array!
+  
+  Players will be stored in objects.
+
+  Control game-flow with an object.
+*/
