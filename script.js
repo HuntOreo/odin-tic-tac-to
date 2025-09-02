6c60ea05924e13f86b742848e86e8fdfaf8509de
HuntOreo <hloreair@outlook.com>
2025-09-01 23:15:33 -0400

    Build board, set board size, build tile

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-console.log('Hello world!');
 /* 
 Main Goal:
   Build a functional Tic Tac Toe game.
@@ -16,3 +15,76 @@ Requirements:
 
   Control game-flow with an object.
 */
+
+const gameBoard = (function () {
+  let board = [];
+  let boardSize = 3;
+  let boardString = "";
+
+  function tileFactory(marker, row, column) {
+    let player = null;
+    function build() {
+      return {
+        marker,
+        row,
+        column,
+        player
+      }
+    }
+
+    return {
+      build,
+    }
+  }
+
+  function buildBoard(size) {
+    let tiles = [];
+    let index = 0;
+    for (let i = 0; i < size; i++) {
+      let row = [];
+      for (let j = 0; j < size; j++) {
+        const rowIndex = i;
+        const colIndex = j;
+        const tileBlueprint = tileFactory('.', rowIndex, colIndex);
+        const tile = tileBlueprint.build();
+        row.push(tile);
+        index++;
+      }
+
+      tiles.push(row);
+    }
+    return tiles;
+  }
+
+  function init(size = 3) {
+    boardSize = size;
+    const builtBoard = buildBoard(size);
+    board.push(...builtBoard);
+  }
+
+  function getSize() {
+    console.log(`Board is ${boardSize} by ${boardSize}`);
+  }
+
+  function displayBoard() {
+    boardString = "";
+    for (let i = 0; i < boardSize; i++) {
+      for (let j = 0; j < boardSize; j++) {
+        boardString += `[${board[i][j].marker}]`;
+      }
+      boardString += `\n`
+    }
+
+    return boardString;
+  }
+
+  return {
+    init,
+    getSize,
+    displayBoard,
+  }
+})()
+
+gameBoard.init(10);
+gameBoard.getSize();
+console.log(gameBoard.displayBoard());
