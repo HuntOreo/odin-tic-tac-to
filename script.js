ce0a3845b6ecc27a11a46a8b045a8a6454ceff60
HuntOreo <hloreair@outlook.com>
2025-09-02 00:15:30 -0400

    Reduce Objective comment

--- script.js
+++ script.js
@@ -2,18 +2,6 @@
 Main Goal:
   Build a functional Tic Tac Toe game.
   Use as little globally scoped code as possible.
- 
-Requirements:
-  Use Factory Functions
-    For single instance objects like game-board, 
-    or displayContainer, use IIFE module patterns so that 
-    it cannot be reused to create additional instances.
-  
-  Store the game-board inside of an array!
-  
-  Players will be stored in objects.
-
-  Control game-flow with an object.
 */
 
 // GAMEBOARD IIFE //
